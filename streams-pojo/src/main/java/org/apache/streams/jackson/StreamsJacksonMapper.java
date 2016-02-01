4783180c66dc3d1e5b02a8077c3654e066896e7f
@steveblackmon <sblackmon@apache.org>
2016-02-01 16:52:52 -0600

    should resolve PDB-540

--- streams-pojo/src/main/java/org/apache/streams/jackson/StreamsJacksonMapper.java
+++ streams-pojo/src/main/java/org/apache/streams/jackson/StreamsJacksonMapper.java
@@ -90,9 +90,11 @@ public class StreamsJacksonMapper extends ObjectMapper {
     public void configure() {
         disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, Boolean.FALSE);
+        configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, Boolean.FALSE);
         configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, Boolean.TRUE);
         configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, Boolean.TRUE);
         configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, Boolean.TRUE);
+        configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, Boolean.TRUE);
         configure(DeserializationFeature.WRAP_EXCEPTIONS, Boolean.FALSE);
         configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, Boolean.TRUE);
         // If a user has an 'object' that does not have an explicit mapping, don't cause the serialization to fail.
